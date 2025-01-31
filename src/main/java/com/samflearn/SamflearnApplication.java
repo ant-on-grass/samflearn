114ad34cd0c70f1aae5769d8f4daef17ccc1ddf7
TE <gimt75257@gmail.com>
2025-01-31 17:07:58 +0900

    x

---
+++ src/main/java/com/samflearn/SamflearnApplication.java
@@ -0,0 +1,13 @@
+package com.samflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamflearnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SamflearnApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/samflearn/SamflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamflearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

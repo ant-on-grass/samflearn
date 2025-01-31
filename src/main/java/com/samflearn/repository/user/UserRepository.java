18fd2fd4e53af514c100d9b7ce3adc5323d51a8f
JinsolSong <jeanies@naver.com>
2025-01-31 21:35:37 +0900

    :sparkles: feat : 유저 생성 기능 구현

--- src/main/java/com/samflearn/repository/user/UserRepository.java
+++ src/main/java/com/samflearn/repository/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.samflearn.repository.user;
 
-public class UserRepository {
+import com.samflearn.common.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/samflearn/service/user/UserService.java
+++ src/main/java/com/samflearn/service/user/UserService.java
@@ -1,5 +1,30 @@
 package com.samflearn.service.user;
 
+import com.samflearn.common.entity.User;
+import com.samflearn.dto.user.UserRequestDto;
+import com.samflearn.dto.user.UserResponseDto;
+import com.samflearn.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserResponseDto createUserService(UserRequestDto requestDto) {
+
+        User user = new User(
+                requestDto.getName(),
+                requestDto.getEmail(),
+                requestDto.getPassword(),
+                requestDto.getUserRole()
+        );
+
+        User saveUser = userRepository.save(user);
+
+        return UserResponseDto.toDto(saveUser);
+    }
 }

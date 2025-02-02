a62f5e2822b3186c0584d1614ecb10c6003b1f71
ww313 <wlsghddl3358@naver.com>
2025-02-03 00:03:56 +0900

    :fire: fix : 쓸데없는 코드 제거했습니다.

--- src/main/java/com/samflearn/repository/course/CourseRepository.java
+++ src/main/java/com/samflearn/repository/course/CourseRepository.java
@@ -3,8 +3,5 @@ package com.samflearn.repository.course;
 import com.samflearn.common.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CourseRepository extends JpaRepository<Course,Long> {
-    Optional<Course> findById(Long Id);
 }

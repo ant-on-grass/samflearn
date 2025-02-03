7b90d7e1df5135e0741ce12da258399325a7a864
JinsolSong <jeanies@naver.com>
2025-02-03 09:21:18 +0900

    :sparkles: feat : 좋아요 기능 구현
             1. 좋아요 생성 기능
             2. 좋아요 삭제 기능

--- src/main/java/com/samflearn/controller/like/LikeController.java
+++ src/main/java/com/samflearn/controller/like/LikeController.java
@@ -1,5 +1,38 @@
 package com.samflearn.controller.like;
 
+import com.samflearn.service.like.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/likes")
+@RequiredArgsConstructor
 public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/{userId}/{courseId}")
+    public ResponseEntity<Void> createLike(
+            @PathVariable Long userId,
+            @PathVariable Long courseId
+    ) {
+        likeService.createLikeService(userId, courseId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{likeId}/{userId}/{courseId}")
+    public ResponseEntity<Void> deleteLike(
+            @PathVariable Long likeId,
+            @PathVariable Long userId,
+            @PathVariable Long courseId
+    ) {
+        likeService.deleteLikeService(likeId, userId, courseId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/samflearn/service/like/LikeService.java
+++ src/main/java/com/samflearn/service/like/LikeService.java
@@ -1,5 +1,49 @@
 package com.samflearn.service.like;
 
+import com.samflearn.common.entity.like.Like;
+import com.samflearn.common.entity.user.User;
+import com.samflearn.common.exception.user.NotFoundException;
+import com.samflearn.repository.course.CourseRepository;
+import com.samflearn.repository.like.LikeRepository;
+import com.samflearn.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LikeService {
 
+    private final UserRepository userRepository;
+//    private final CourseRepository courseRepository;
+    private final LikeRepository likeRepository;
+
+    public void createLikeService(Long userId, Long courseId) {
+
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다."));
+
+        Course findCourse = courseRepository.findById(courseId)
+                .orElseThrow(()-> new NotFoundException("존재하지 않는 강의 입니다."));
+
+        if(likeRepository.existsByUserAndCourse(findUser, findCourse)) {
+            throw new RuntimeException("이미 좋아요를 눌렀습니다"); //익셉션 핸들러로 변경
+        }
+
+        Like like = new Like();
+
+        like.addUser(findUser);
+
+        like.addCourse(findCourse);
+
+        likeRepository.save(like);
+
+    }
+
+    public void deleteLikeService(Long likeId, Long userId, Long courseId) {
+
+        Like findLike = likeRepository.findById(likeId)
+                .orElseThrow(() -> new NotFoundException("좋아요를 누르지 않은 강의 입니다"));
+        likeRepository.delete(findLike);
+
+    }
 }

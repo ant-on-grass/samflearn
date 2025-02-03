7e9fe07b66ac66fec6f464ed3a34c66625170686
wls313 <71283522+wls313@users.noreply.github.com>
2025-02-03 09:57:01 +0900

    Delete src/main/java/com/samflearn/repository/user directory

--- src/main/java/com/samflearn/repository/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.samflearn.repository.user;
-
-import com.samflearn.common.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}

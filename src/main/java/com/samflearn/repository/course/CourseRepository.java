b171a1cb8d0e83eb1a5abf0df138e51ab9c9d3cb
ww313 <wlsghddl3358@naver.com>
2025-02-03 09:39:50 +0900

    :recycle: fix : queryDsl 사용 위치를 service 에서 repository 로 변경했습니다.

--- src/main/java/com/samflearn/repository/course/CourseRepository.java
+++ src/main/java/com/samflearn/repository/course/CourseRepository.java
@@ -3,5 +3,5 @@ package com.samflearn.repository.course;
 import com.samflearn.common.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course,Long> {
+public interface CourseRepository extends JpaRepository<Course,Long>,CourseQueryDslRepository {
 }
--- src/main/java/com/samflearn/service/course/CourseService.java
+++ src/main/java/com/samflearn/service/course/CourseService.java
@@ -1,14 +1,11 @@
 package com.samflearn.service.course;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.samflearn.common.entity.Course;
 import com.samflearn.common.entity.User;
 import com.samflearn.dto.course.CourseRequestDto;
 import com.samflearn.dto.course.CourseResponseDto;
 import com.samflearn.dto.course.CourseUpdateResponseDto;
 import com.samflearn.repository.course.CourseRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,42 +13,28 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.samflearn.common.entity.QCourse.course;
-import static com.samflearn.common.entity.QUser.user;
-
-
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
     private final CourseRepository courseRepository;
 
-    @PersistenceContext
-    EntityManager em;
-    JPAQueryFactory queryFactory;
-
     public Course createCourse(CourseRequestDto requestDto) {
-        queryFactory = new JPAQueryFactory(em);
+        User user = courseRepository.findUserById(requestDto.getUser_id());
 
-        User fetchOne = queryFactory
-                .selectFrom(user)
-                .where(user.id.eq(requestDto.getUser_id()))
-                .fetchOne();
-
-        Course course = new Course(requestDto.getCourse_name(), requestDto.getCourse_price(), requestDto.getCategory(),fetchOne);
+        Course course = new Course(
+                requestDto.getCourse_name(),
+                requestDto.getCourse_price(),
+                requestDto.getCategory(),
+                user
+        );
 
         return courseRepository.save(course);
     }
 
     @Transactional
     public List<CourseResponseDto> findAllCourse() {
-        queryFactory = new JPAQueryFactory(em);
-
-        List<Course> courseList = queryFactory
-                .selectFrom(course)
-                .fetch();
-
-        return courseList.stream()
+        return courseRepository.findAllCourses().stream()
                 .map(CourseResponseDto::new)
                 .collect(Collectors.toList());
     }
@@ -59,30 +42,19 @@ public class CourseService {
     @Transactional
     public List<CourseResponseDto> findLikeCourse(String course_name)
     {
-        queryFactory = new JPAQueryFactory(em);
-
-        List<Course> courseList = queryFactory
-                .selectFrom(course)
-                .where(course.course_name.like("%" + course_name +"%"))
-                .fetch();
-
-        return courseList.stream()
+        return courseRepository.findLikeCourse(course_name).stream()
                 .map(CourseResponseDto::new)
                 .collect(Collectors.toList());
     }
 
     @Transactional
     public CourseUpdateResponseDto updateCourse(Long id, CourseRequestDto requestDto) {
-        queryFactory = new JPAQueryFactory(em);
 
-        Course findCourse = queryFactory
-                .selectFrom(course)
-                .where(course.id.eq(id))
-                .fetchOne();
+        Course findCourse = courseRepository.findCourseById(id);
         findCourse.updateCourse(requestDto);
-        Course updateCourse = courseRepository.save(findCourse);
+        Course updatedCourse = courseRepository.save(findCourse);
 
-        return CourseUpdateResponseDto.courseUpdateResponseDto(updateCourse);
+        return CourseUpdateResponseDto.courseUpdateResponseDto(updatedCourse);
     }
 
     public void deleteCourse(Long id) {

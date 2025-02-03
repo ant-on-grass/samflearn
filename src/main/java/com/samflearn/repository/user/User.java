30f0487a79a5dd3d8ac3d16720447fb36e2fb8bc
ww313 <wlsghddl3358@naver.com>
2025-02-03 10:09:13 +0900

    유저 클래스 추가

--- src/main/java/com/samflearn/repository/user/User.java
+++ src/main/java/com/samflearn/repository/user/User.java
@@ -1,4 +1,8 @@
+
 package com.samflearn.repository.user;
 
-public class User {
+import com.samflearn.common.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }

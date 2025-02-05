47ac68b23047b120c58954b6bd99681865911886
TE <xodms128@naver.com>
2025-02-05 14:53:19 +0900

    :sparkles:setting: hikari

--- src/main/java/com/samflearn/controller/course/CourseController.java
+++ src/main/java/com/samflearn/controller/course/CourseController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/courses")
 @RequiredArgsConstructor
 public class CourseController {
     private final CourseService courseService;
